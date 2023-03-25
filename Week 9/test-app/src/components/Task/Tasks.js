524ec67dc5b9ba0896f670dd3740f399f8dfa957
ramseyFHSU <ramsey.fhsu@gmail.com>
2023-03-25 11:39:57 -0500

    Week 9

---
+++ Week 9/test-app/src/components/Task/Tasks.js
@@ -0,0 +1,36 @@
+import React, { useState, useContext } from "react";
+import TaskContext from "../context/TaskContext";
+import SearchTask from "./SearchTask";
+import Task from "./Task";
+
+export default function Tasks() {
+  const [search, setSearch] = useState("");
+
+  const { taskList } = useContext(TaskContext);
+
+  const result = taskList.filter((task) =>
+    task.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <>
+      <SearchTask search={search} setSearch={setSearch} />
+      {result.length ? (
+        <div>
+          {result.map((task) => (
+            <Task
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              description={task.description}
+              checked={task.checked}
+              task={task}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>Task List is empty</p>
+      )}
+    </>
+  );
+}
---
+++ Week 9/test-app/src/pages/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <h1>Welcome to Task Manager</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signIn">SingIn</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
